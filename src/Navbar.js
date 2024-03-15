ccbd7c1af13e8a71e01caefb57789e6a69ea7088
RGvirer <118rgv@gmail.com>
2024-03-15 04:06:53 +0200

    logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
     return (
         <AppBar position="sticky" sx={{ boxShadow: 'none', direction: 'rtl', position: 'sticky', left: 0, zIndex: 100, marginBottom: "2%" }}>
             <Link to={"/"} style={{ margin: 'auto' }}>
-                <img alt='logo' style={{ width: "70%", marginTop: "10px", marginBottom: "-15px" }} src={`https://storeserver-uoax.onrender.com/לוגו-removebg-preview.png`} />
+                <img alt='logo' style={{ width: "60%",marginRight:"15%" ,marginBottom:"-7%"}} src={`https://storeserver-uoax.onrender.com/לוגו-removebg-preview.png`} />
             </Link>
             <Toolbar style={{ justifyContent: 'center' }}>
                 <div style={{ border: "2px solid white", padding: "1%", borderRadius: "15%" }}>{username}</div>
